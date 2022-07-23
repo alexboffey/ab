baa4467e522c937751e194e20791d6aa45e88c80
Alex Boffey <alex@alexboffey.co.uk>
2022-07-23 23:18:14 +0100

    Workspace setup with storybook

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,15 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import {Button} from "@vite-workspaces/ui"
-
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Button backgroundColor='white'>Hey</Button>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
