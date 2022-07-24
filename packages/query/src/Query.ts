8a98fe7c389c68ddef09877debbcfe374bd37285
Alex Boffey <alex@alexboffey.co.uk>
2022-07-24 15:30:37 +0100

    add seperate package

---
+++ packages/query/src/Query.ts
@@ -0,0 +1,43 @@
+// Main async util wrapper
+export type Input<T> = (v: T) => Promise<T>;
+const query = <T>(input: Input<T>, all: Input<T>[]) => {
+  return {
+    value: () => input,
+    all: () => all,
+    chain: <K extends T>(nextInput: (i: T) => Promise<K>) => {
+      // @ts-ignore
+      return query<K>(nextInput, all.concat(nextInput));
+    },
+    use: <K extends T>(f: Input<T>) => {
+      // @ts-ignore
+      return query<K>(input, all.map(f));
+    },
+    // @ts-ignore
+    resolve: (res = (t: T) => t, rej = (t: T, e: T) => t) => {
+      const resolveAll = async (val: T, i = 0): Promise<T> => {
+        if (!all[i]) {
+          // @ts-ignore
+          return val;
+        }
+
+        return await all[i](val)
+          .then((v) => {
+            // res(v);
+            return resolveAll(v, i + 1);
+          })
+          .catch((err) => {
+            // rej(val, err);
+            return resolveAll(val, i + 1);
+          });
+      };
+
+      // @ts-ignore
+      return resolveAll(null, 0);
+    },
+  };
+};
+
+// API to expose async util
+export const Query = {
+  of: <T>(input: () => Promise<T>) => query(input, [input]),
+};
