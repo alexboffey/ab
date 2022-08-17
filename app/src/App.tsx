6b4c8f202566a137af17b3b0baa249e0bf6000ff
Alex Boffey <alex@alexboffey.co.uk>
2022-08-17 22:20:36 +0100

    Basic FE data dump

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,29 +1,39 @@
 import { useEffect, useState } from "react";
-import { Button } from "@ab/ui";
+import { Pokemon } from "../../api/data/schema";
 
 function App() {
-  const [val, setVal] = useState({});
-  const [count, setCount] = useState(0);
+  const [val, setVal] = useState<Pokemon[]>([]);
+  // const [count, setCount] = useState(0);
 
   useEffect(() => {
     fetch("/api/pokemon")
       .then((r) => r.json())
-      .then((r) => setVal(r));
+      .then((r) => setVal(r.data.pokemon));
   });
 
   return (
-    <div className="App">
-      <pre
+    <main className="App">
+      {val.map((p) => {
+        return (
+          <div key={p.id} style={{ background: "#e1f1e1" }}>
+            <p>{p.name}</p>
+            <p>#{p.num}</p>
+            <img src={p.img} alt={p.name} />
+            <pre
+              style={{ height: "10rem", width: "30rem", overflowY: "scroll" }}
+            >
+              <code>{JSON.stringify(p, null, 2)}</code>
+            </pre>
+            {/* <img src={p.image} alt={p.name} /> */}
+          </div>
+        );
+      })}
+      {/* <pre
         style={{ height: "20rem", overflowY: "scroll", background: "#f8f1f1" }}
       >
         <code> {JSON.stringify(val, null, 2)}</code>
-      </pre>
-      <Button
-        backgroundColor="white"
-        label={`count is ${count}`}
-        onClick={() => setCount((count) => count + 1)}
-      ></Button>
-    </div>
+      </pre> */}
+    </main>
   );
 }
 
