9c91482d5271fce83dacc6f5159fa57e64a17771
Alex Boffey <alex@alexboffey.co.uk>
2022-08-20 01:06:51 +0100

    connect fe

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,14 +7,13 @@ function App() {
   useEffect(() => {
     fetch("/api/pokemon")
       .then((r) => r.json())
-      .then((r) => setPokemon(r.data.pokemon));
+      .then((r) => setPokemon(r.data));
   });
 
   return (
     <div>
       <p>Number of Pokemon: {pokemon.length}</p>
-
-      <div className="grid grid-cols-5 gap-8 bg-slate-100 p-8">
+      <div className="grid grid-cols-4 gap-8 bg-slate-100 p-8">
         {pokemon.map((poke) => {
           return (
             <div key={poke.id} className="bg-white shadow-lg rounded-lg p-8">
